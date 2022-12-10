d1bc0abca82ba2565dc81f7e7e97e85c1b4e9bfc
odamaharris <odamaharris@gmail.com>
2022-12-09 16:14:04 -0800

    8-print_base16.c task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,7 +3,7 @@
 /**
  * main -  prints all the numbers of base 16 in lowercase
  *
- * Return: Always 0
+ * Reurn: Always 0
  */
 
 int main(void)
@@ -13,13 +13,13 @@ int main(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		putchar(n);
+		putchar((n % 10) + '0');
 	}
 	for (a = 'a'; a <= 'f'; a++)
 	{
-		putchar((n % 10) + '0');
+		putchar(a);
 	}
-	putchar ('\n');
+	putchar('\n');
 
 	return (0);
 }
