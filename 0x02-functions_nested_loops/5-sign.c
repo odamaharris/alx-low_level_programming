bf22b224ee28493dfb12f89dd82cfaafba23858d
odamaharris <odamaharris@gmail.com>
2022-12-13 15:04:11 -0800

    5-sign.c task

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- *  print_sign -  prints the sign of a number
+ * print_sign -  prints the sign of a number
  *
- *  Return: 
+ * @n: represent numbers
+ *
+ * Return: according to code
  */
 
 int print_sign(int n)
@@ -13,11 +15,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-		else if (n == 0)
-		{
-			_putchar('0');
-			return (0);
-		}
-		else
-			_putchar('-');
-			return (-1);
+	else if (n == 0)
+	{
+		_putchar('0);
+		return (0);
+	}
+	else
+		_putchar(' - ');
+	return (-1)
+}
