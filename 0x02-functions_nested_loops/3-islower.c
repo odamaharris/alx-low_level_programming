b99b463b98682b57bd1cf21aaa92bdebdadcd2b6
odamaharris <odamaharris@gmail.com>
2022-12-13 07:35:25 -0800

    3-islower.c task

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  _islower -  checks for lowercase character
+ *
+ *  Return: Always 0
+ */
+
+int _islower(int c)
+{
+	char c = 'c';
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}
