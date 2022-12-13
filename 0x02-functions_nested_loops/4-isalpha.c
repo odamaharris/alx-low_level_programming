51da0ff32a295c94eb2dea9831d404946714d925
odamaharris <odamaharris@gmail.com>
2022-12-13 08:14:35 -0800

    4-isalpha.c task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isalpha - checks if character is an alphabet
+ *
+ * @c: represent alphabet
+ *
+ * Return: 1 if its alphabet, 0 if its not alphabet
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 'A' && c <= 'z'; c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
