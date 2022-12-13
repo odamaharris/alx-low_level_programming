02602f4d9827b0b1e2c173ff2dd536b4d99dd98c
odamaharris <odamaharris@gmail.com>
2022-12-13 05:40:32 -0800

    2-print_alphabet_x10.c task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lower case 10 times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a) * 10;
+	}
+	_putchar('\n');
+}
