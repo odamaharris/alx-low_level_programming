ffa10cbf04d079494f4157c8aa7e827614d30060
odamaharris <odamaharris@gmail.com>
2022-12-15 12:45:03 -0800

    0-main.c, main.h task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test function that test if number is nrgative or positive
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
