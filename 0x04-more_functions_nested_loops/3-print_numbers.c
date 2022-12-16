7c83d2ba13b2441b8b200e96d4df8c2fb3537fc8
odamaharris <odamaharris@gmail.com>
2022-12-15 16:41:09 -0800

    3-print_numbers.c task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i++ <= 9)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
