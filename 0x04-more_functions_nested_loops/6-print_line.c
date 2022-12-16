cc4136b48db7bb97145f155b63e965da13e037de
odamaharris <odamaharris@gmail.com>
2022-12-16 06:12:04 -0800

    6-print_line.c task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,12 +8,13 @@
 
 void print_line(int n)
 {
-	int len;
+	int l;
 
-	if (n > 0)
-	{
-		for (len = 0; len < n; len++)
-			_putchar('_');
-	}
+	if (n <= 0)
+		_putchar('\n');
+
+		for (l = 0; len <= n; len++)
+			_putchar(95);
+}
 	_putchar('\n');
 }
