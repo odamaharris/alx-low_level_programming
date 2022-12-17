821cb0bdcbbad11509ebe561ad51c2913ed41669
odamaharris <odamaharris@gmail.com>
2022-12-16 16:57:45 -0800

    8-print_square.c task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,9 +16,9 @@ void print_square(int size)
 		_putchar('\n');
 	else
 	{
-		for (a = 0; a <= size; a++)
+		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b <= size; b++)
+			for (b = 0; b < size; b++)
 			{
 				_putchar(35);
 			}
