ca6e139a94c0d5975373075818ae69825ae95236
odamaharris <odamaharris@gmail.com>
2022-12-16 17:26:22 -0800

    9-fizz_buzz.c task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print number 1 to 100
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else if ((i % 15) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+	printf(" ");
+	}
+	return (0);
+}
