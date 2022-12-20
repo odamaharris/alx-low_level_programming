565697ae86f0eb9a5356e81e0c357a9442ba383a
odamaharris <odamaharris@gmail.com>
2022-12-19 16:03:49 -0800

    puts task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout.
+ *
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
