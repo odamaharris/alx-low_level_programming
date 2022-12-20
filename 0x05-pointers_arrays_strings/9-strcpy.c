457c721b19741da225f79d7fb1cd2ca4e4be5334
odamaharris <odamaharris@gmail.com>
2022-12-19 18:15:10 -0800

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,8 +4,8 @@
 /**
  * print_array - prints n elements of an array of integers
  *
- * @a: pointer
- * @n: number of element in array
+ * @dest: pointer
+ * @src: number of element in array
  *
  * Return: void
  */
@@ -13,6 +13,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
+
 	while (count >= 0)
 	{
 		*(dest + count) = *(src + count);
