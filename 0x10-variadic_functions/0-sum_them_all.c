7706eac0f2c52cde58697845efd5f3277c84fbab
odamaharris <odamaharris@gmail.com>
2023-01-25 17:56:05 -0800

    0-sum_them_all.c task correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,13 +12,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list nums;
-	unsigned int i, sum = 0;
+	unsigned int i;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	va_start(nums, n);
-	for (i = 0; index < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(nums, int);
 	va_end(nums);
 
