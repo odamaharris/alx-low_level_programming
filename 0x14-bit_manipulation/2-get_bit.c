eca681c699f21a31f479c27a69122e2df691ba27
odamaharris <odamaharris@gmail.com>
2023-02-02 09:08:56 -0800

    Bit Manipulations

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * get_bit - Gets the value of bit
+ *
+ * @n: representing bit
+ * @index: representing index
+ *
+ * Return: If error - -1. else - value of bit
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	if ((n & (1 << index)) == 0)
+		return (0);
+	return (1);
+}
