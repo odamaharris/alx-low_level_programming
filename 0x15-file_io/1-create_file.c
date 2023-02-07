071aac1e62d726c5a7a55e7bd7b5a544c1d413c6
odamaharris <odamaharris@gmail.com>
2023-02-06 16:56:54 -0800

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ *
+ * @filename: A pointer to the name of the file
+ * @text_content: points to a string to write to file
+ *
+ * Return: If the function fails - -1 elsr - 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, l = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (l = 0; text_content[l];)
+			l++;
+	}
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, l);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
